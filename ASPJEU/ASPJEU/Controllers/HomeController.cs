0151ed49bceb8738a85c96b52681d0174857a4f5
ProjetInfoAlexandre <apytio3@gmail.com>
2018-01-04 17:37:41 +0100

    Le vrai premier push

--- ASPJEU/ASPJEU/Controllers/HomeController.cs
+++ ASPJEU/ASPJEU/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace ASPJEU.Controllers
 
             return View();
         }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "test";
+
+            return View();
+        }
     }
 }
